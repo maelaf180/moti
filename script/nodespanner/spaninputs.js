c5b1b8c478ead75906a49977eab022cdada2814a
ketsebaotgizachew <112580228+ketsebaotgizachew@users.noreply.github.com>
2024-04-09 18:19:42 +0300

    fixed input node

---
+++ script/nodespanner/spaninputs.js
@@ -0,0 +1,8 @@
+// let node_container = document.querySelector(".main-note");
+// let typing_note_count = 20;
+
+// for(let i=0;i<typing_note_count;i++){
+//     let typing_node = document.createElement("div");
+//     typing_node.classList.add("input-node node");
+//     let move_pointer_btn = document.createElement("button");
+// }

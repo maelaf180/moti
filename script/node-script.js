43ba78cfca090b47c6baebd2b583a065bc91f8af
ketsebaotgizachew <112580228+ketsebaotgizachew@users.noreply.github.com>
2024-04-09 15:48:38 +0300

    something

---
+++ script/node-script.js
@@ -0,0 +1,32 @@
+
+//all input fields must have this class so there buttons are color changed when they are in focus
+let node = document.querySelectorAll(".node");
+let should_pen_highlight = true;
+let active_pen_button;
+let active_input_field;
+
+node.forEach(elements => {
+    let input_holder = elements.querySelector(".input-holder")
+    input_holder.addEventListener("focus",()=>{
+        let pen_button_holder = elements.querySelector(".pen-button-holder");
+        let pen_button = pen_button_holder.querySelector(".pen-button")
+        active_pen_button = pen_button;
+        active_input_field = input_holder;
+        active_input_field.addEventListener("input",function(){
+            should_pen_highlight = false;
+            active_pen_button.style.fill = "transparent";
+            console.log("fired")
+        })
+    })
+});
+
+
+
+document.addEventListener("mousemove",function(){
+    should_pen_highlight = true;
+    if(should_pen_highlight){
+        if(active_pen_button){
+            active_pen_button.style.fill = "grey";
+        }
+    }
+})
